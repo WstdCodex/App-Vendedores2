a7478c0dbd1b2f978328cf9b441ce954b5103836
LucasWstd <lucasmedinawstd@gmail.com>
2025-09-11 09:02:27 -0300

    cambio de puerto

--- app.py
+++ app.py
@@ -578,4 +578,4 @@ def api_facturas_cliente(cliente_id):
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5200)
