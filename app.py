877c928523c4b65025fec3f2f21e5b83c5427c74
unknown <maximo.uriel76@gmail.com>
2025-09-11 10:56:55 -0300

    static

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path = '/vendedores')
 app.secret_key = 'tu_clave_secreta_aqui'  # Cambiar por una clave segura
 
 
